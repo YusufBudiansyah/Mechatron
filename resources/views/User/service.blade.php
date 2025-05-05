4cab8f1cb69f8e3cd18337069ed556004776b850
YusufBudiansyah <yusufbbb444@gmail.com>
2025-05-05 22:31:55 +0700

    fixing bug

--- resources/views/User/service.blade.php
+++ resources/views/User/service.blade.php
@@ -39,45 +39,6 @@
         </div>
     </section>
 
-    <!-- <div class="container py-4 my-5">
-        <div class="row mb-4 pb-2">
-            <div class="col">
-                <p class="text-4 font-weight-semibold mb-4 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="300">Get reliable & affordable auto services for your facility with a 100% satisfaction guaranteed! </p>
-                <p class="pb-1 mb-3 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin bibendum ultricies nunc, eu interdum enim convallis pretium. Quisque eu neque augue. Aliquam egestas nunc at efficitur faucibus. Praesent mauris eros, tincidunt id enim sodales, rhoncus malesuada ligula. </p>
-                <p class="mb-1 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="700">Vivamus quis purus nec sapien pellentesque imperdiet. Nullam porttitor augue mi, sit amet luctus est tincidunt sed. Donec tempus bibendum ex, nec vehicula elit. </p>
-            </div>
-        </div>
-        <div class="row row-gutter-sm mb-3 appear-animation" data-appear-animation="fadeInUpShorterPlus" data-appear-animation-delay="950">
-            <div class="col-sm-6 col-lg-3 text-center mb-4">
-                <a href="/stage" class="text-decoration-none">
-                    <div class="custom-thumb-info-style-1 thumb-info thumb-info-no-borders thumb-info-no-borders-rounded thumb-info-lighten">
-                        <div class="thumb-info-wrapper">
-                            <img src="assets/img/demos/auto-services/services/service-small-1.jpg" class="img-fluid" alt="">
-                        </div>
-                        <h3 class="text-transform-none font-weight-bold text-5 mt-2 mb-0">Stage Level</h3>
-                    </div>
-                </a>
-            </div>
-            <div class="col-sm-6 col-lg-3 text-center mb-4">
-                <a href="/activities" class="text-decoration-none">
-                    <div class="custom-thumb-info-style-1 thumb-info thumb-info-no-borders thumb-info-no-borders-rounded thumb-info-lighten">
-                        <div class="thumb-info-wrapper">
-                            <img src="assets/img/demos/auto-services/services/service-small-2.jpg" class="img-fluid" alt="">
-                        </div>
-                        <h3 class="text-transform-none font-weight-bold text-5 mt-2 mb-0">Activities & Programs</h3>
-                    </div>
-                </a>
-            </div>
-            <div class="col-sm-6 col-lg-3 text-center mb-4">
-                <a href="/hardware" class="text-decoration-none">
-                    <div class="custom-thumb-info-style-1 thumb-info thumb-info-no-borders thumb-info-no-borders-rounded thumb-info-lighten">
-                        <div class="thumb-info-wrapper">
-                            <img src="assets/img/demos/auto-services/services/service-small-3.jpg" class="img-fluid" alt="">
-                        </div>
-                        <h3 class="text-transform-none font-weight-bold text-5 mt-2 mb-0">Hardware & Programming</h3>
-                    </div>
-                </a>
-            </div> -->
 
     <div class="container py-4 my-5">
         <div class="row mb-4 pb-2">
